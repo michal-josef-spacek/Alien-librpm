b60bba9f1c6215a4ce622ba2fb1fffc41ed59a5e
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-06-23 18:35:19 +0200

    Update example output

--- examples/alien_librpm_variables.pl
+++ examples/alien_librpm_variables.pl
@@ -12,5 +12,10 @@ print 'libs: '.Alien::librpm->libs."\n";
 print 'libs_static: '.Alien::librpm->libs_static."\n";
 print 'version: '.Alien::librpm->version."\n";
 
-# Output like:
-# TODO
+# Output like (Debian 11.7 system rpm library):
+# cflags:  
+# cflags_static:  
+# dist_dir: ../perl5/x86_64-linux-gnu-thread-multi/auto/share/dist/Alien-librpm
+# libs: -lrpm -lrpmio 
+# libs_static: -lrpm -lrpmio -lpopt -lrt -lpthread -llzma -ldb -lbz2 -lz -llua5.2 -lzstd 
+# version: 4.16.1.2
