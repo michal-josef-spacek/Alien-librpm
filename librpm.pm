238f6272826ff8a5d60a376625ff1ffe95a7bb8c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-06-23 18:35:03 +0200

    Prepare for 0.02 version

--- librpm.pm
+++ librpm.pm
@@ -4,7 +4,7 @@ use parent qw(Alien::Base);
 use strict;
 use warnings;
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 1;
 
@@ -92,6 +92,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
--- t/Alien-librpm/02-version.t
+++ t/Alien-librpm/02-version.t
@@ -6,4 +6,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($Alien::librpm::VERSION, 0.01, 'Version.');
+is($Alien::librpm::VERSION, 0.02, 'Version.');
